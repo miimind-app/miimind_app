411735491a02ce460e84207e045afe664ced2f77
Mi≈iMind <tonywicklow@gmail.com>
2026-09-25 10:17:26 +0000

    Add Filament 5 admin panel foundation

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -12,8 +14,12 @@ use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
-{
+class User extends Authenticatable implements FilamentUser
+    {
+    public function canAccessPanel(Panel $panel): bool
+    {
+    return $panel->getId() === 'admin';
+    }
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
